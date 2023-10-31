d0fdc1a22a52d45b177d116a810bbe1682e32f86
lpzkjdoi <maxime.chazard@le-campus-numerique.fr>
2023-10-31 17:06:13 +0100

    main décommenté

--- src/fr/lecampusnumerique/main/Main.java
+++ src/fr/lecampusnumerique/main/Main.java
@@ -8,26 +8,10 @@ import fr.lecampusnumerique.main.Menu;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, PersonnageHorsPlateauException, MauvaisChoixUtilisateur {
+    public static void main(String[] args) throws SQLException, MauvaisChoixUtilisateur, PersonnageHorsPlateauException {
 
         Controller newController = new Controller();
         newController.start();
-//
-//
-//
-//
-//
-//
-//        try {
-
-//        Menu menu = new Menu();
-
-//        } catch (SQLException e) {
-//            System.out.println("Déso ta méthode pue sa mère");
-//        } catch (PersonnageHorsPlateauException e) {
-//            throw new RuntimeException(e);
-//        }
-
 
     }
 }
